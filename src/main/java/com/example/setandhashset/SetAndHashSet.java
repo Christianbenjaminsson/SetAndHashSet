b20789ed80f30bf0ff39744eefbbd20e2eb0779c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-12 21:32:22 +0200

    Add planet Pluto

--- src/main/java/com/example/setandhashset/SetAndHashSet.java
+++ src/main/java/com/example/setandhashset/SetAndHashSet.java
@@ -94,5 +94,12 @@ public class SetAndHashSet {
         for(HeavenlyBody moon : moons) {
             System.out.println("\t" + moon.getName());
         }
+
+        HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
+        planets.add(pluto);
+
+        for (HeavenlyBody planet : planets) {
+            System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
+        }
     }
 }
