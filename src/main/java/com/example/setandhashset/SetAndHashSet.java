ecf3cca6801332200f178ff5e119e20592bc8225
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-13 18:28:22 +0200

    Created method equals

--- src/main/java/com/example/setandhashset/SetAndHashSet.java
+++ src/main/java/com/example/setandhashset/SetAndHashSet.java
@@ -101,5 +101,9 @@ public class SetAndHashSet {
         for (HeavenlyBody planet : planets) {
             System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
         }
+
+        Object o = new Object();
+        o.equals(0);
+        "Pluto".equals("");
     }
 }
