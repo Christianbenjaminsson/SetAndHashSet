2d5535a75952860021ab41caaf7a7505985e420a
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-22 10:38:07 +0200

    Created class SetMain

--- src/main/java/com/example/setandhashset/SetMain.java
+++ src/main/java/com/example/setandhashset/SetMain.java
@@ -1,8 +1,10 @@
 package com.example.setandhashset;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.Phaser;
 
 public class SetMain {
     public static void main(String[] args) {
@@ -34,5 +36,52 @@ public class SetMain {
         for (String s : words) {
             System.out.println(s);
         }
+
+        Set<String> nature = new HashSet<>();
+        Set<String> divine = new HashSet<>();
+        String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
+        nature.addAll(Arrays.asList(natureWords));
+        String [] divineWords =  {"to", "err", "is", "human", "to", "forgive", "divine"};
+        divine.addAll(Arrays.asList(divineWords));
+
+        System.out.println("nature - divine:");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("divine - nature");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        printSet(diff2);
+
+        Set<String> unionTest = new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String> intersectionTest = new HashSet<>(nature);
+        intersection.retainAll(divine);
+
+        System.out.println("Symmetric difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if (nature.containsAll(divine)) {
+            System.out.println("divine is a subset of nature");
+        }
+
+        if (nature.containsAll(intersectionTest)) {
+            System.out.println("intersection is a subset of nature");
+        }
+
+        if (divine.containsAll(intersectionTest)) {
+            System.out.println("intersection is a subset of divine");
+        }
+    }
+
+    private static void printSet(Set<String> set) {
+        System.out.println("\t");
+        for (String s : set) {
+            System.out.println(s + " ");
+        }
+
+        System.out.println();
     }
 }
